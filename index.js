8cd52e29a52326bc3363c46cdf4871b1ae252e47
Denise Nadal <denisenadal@outlook.com>
2018-03-01 23:05:27 -0700

    adding frontend styles

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 //path to search for images
 app.all(/(\/search)|(\/search\/:terms)/,function(req,res){
 	var url_parts = url.parse(req.url, true);
-    var query = url_parts.query;
+	var page = url_parts.query.offset !== 'undefined' ? url_parts.query.offset : 1;
 	//parse request terms
-	client.get(apiURL+'flickr.photos.search&api_key=' +process.env.API_KEY+ '&media=photos&per_page=10&tags='+ query.terms, function(data,resp){
+	client.get(apiURL+'flickr.photos.search&api_key=' +process.env.API_KEY+ '&media=photos&per_page=10&page='+page+'&tags='+ url_parts.query.terms, function(data,resp){
 		//need to format resp as pic {url,title,thumb,pageURL}
 		//send stuff back to client
 		res.status(200).json(data);
@@ -29,10 +29,12 @@ app.all(/(\/search)|(\/search\/:terms)/,function(req,res){
 
 //path to browse latest images
 app.get('/latest/:page', function(req,res){
-	client.get(apiURL+'flickr.photos.getRecent&api_key=' +process.env.API_KEY+ '&format=json&per_page=10&extras=date_upload,owner_name,url_sq,views,tags', {headers: { "Content-Type": "application/json" }}, function(data,resp){
+	var url_parts = url.parse(req.url, true);
+	var page = url_parts.query.offset !== 'undefined' ? url_parts.query.offset : 1;
+	client.get(apiURL+'flickr.photos.getRecent&api_key=' +process.env.API_KEY+ '&format=json&per_page=10&page='+page+'&extras=date_upload,owner_name,url_sq,views,tags,url_o', {headers: { "Content-Type": "application/json" }}, function(data,resp){
 		//send stuff back to client
 		var jsonFlickrApi = function(data){
-			res.status(200).json(data.photos.photo );
+			res.status(200).json(data.photos );
 		};
 		eval(data.toString());
 	});
--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,31 @@
+Handlebars.registerHelper('formatDate',function(timestamp){
+	return new Date(timestamp * 1000).toLocaleDateString('en-us',{ year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric',minute:'numeric',hour12: true });
+});
+
 var temphtml = $('#photo-template').html();
 var photoTemplate = Handlebars.compile(temphtml);
 
-$.getJSON('./latest',function(photos){
-	console.log(photos);
-	var data = { photos: photos};
-	$('.photo-results').append(photoTemplate(data));
+getLatest();
+
+$('#next').click(function(){
+	var page = $('$photo-results').attr('data-page');
+	$('$photo-results').attr('data-page', page.toInt() + 1);
+	getLatest(page);
 
 });
+
+
+function getLatest(page, success){
+	if(page === 'undefined'){ var page = 1;}
+	$.getJSON('./latest/'+page,function(photos){
+		console.log(photos);
+		var data = { photos: photos.photo};
+		$('#photo-results').html(photoTemplate(data));
+		if(photos.pages < 2){ $('#next').hide(); }
+		else{$('#next').show();}
+		if(photos.page >1){ $('#previous').show(); }
+		else{$('#previous').hide(); }
+	});
+
+
+}
